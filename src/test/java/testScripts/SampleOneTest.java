093dc5b8783edbef55fbfd10f0e9dc589ee7c26d
kiruthigan <kiruthin96@gmail.com>
2023-12-08 10:51:24 +0530

    test

--- src/test/java/testScripts/SampleOneTest.java
+++ src/test/java/testScripts/SampleOneTest.java
@@ -6,15 +6,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SampleOneTest {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setup()
+	{
+		 driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+	}
   @Test
   public void cucumberSearchTest() {
 	  
 	  
-	  WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
+	  
 		driver.get("https://www.google.com");
 		WebElement srcBox = driver.findElement(By.className("gLFyf"));
 		srcBox.sendKeys("cucumber Tutorial");
@@ -31,8 +41,7 @@ public class SampleOneTest {
   public void SeleniumSearchTest() {
 	  
 	  
-	  WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
+	  
 		driver.get("https://www.google.com");
 		WebElement srcBox = driver.findElement(By.className("gLFyf"));
 		srcBox.sendKeys("selenium Tutorial");
